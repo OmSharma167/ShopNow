8d5e86c97d99e672f77e279d883430df513039fb
OmSharma167 <op809034@gmail.com>
2025-08-12 10:40:38 +0530

    Code changes and modify some fun

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="h-40 w-80 bg-blue-100 flex items-center justify-center">
+        <span className="text-red-600 text-2xl font-bold">Tailwind Test</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
